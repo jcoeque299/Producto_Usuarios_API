a16aecc603c347280a7f766441e39b4bcb8af96b
Judith <jcoeque299@g.educaand.es>
2023-11-03 09:11:31 +0100

    Añadido jakarta validation

--- src/main/java/com/example/EjercicioProducto/model/User.java
+++ src/main/java/com/example/EjercicioProducto/model/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,8 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @NotBlank
     private String name;
+    @Email
     private String email;
 }
